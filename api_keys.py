80d272215e268f03445dcfd155d6a61731d450b6
William F <60886603+wtcf90@users.noreply.github.com>
2020-05-16 21:07:41 -0700

    Update api_keys.py
    Removing key information

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "<ENTER YOUR KEY HERE>"
+weather_api_key = "<ENTER KEY HERE>"
 
 # Google API Key
-g_key = "<ENTER YOUR KEY HERE>"
+g_key = "<ENTER KEY HERE>"
