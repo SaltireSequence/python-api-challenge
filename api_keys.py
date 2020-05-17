24797f25d5c21507eb4b0481ac26d77967ea5505
William Forsyth <60886603+SaltireSequence@users.noreply.github.com>
2020-05-16 21:05:25 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "19f617e49bc08fb97137c4f01a36e91d"
+weather_api_key = "<ENTER YOUR KEY HERE>"
 
 # Google API Key
-g_key = "AIzaSyDMuKlBd4oXw9XmvdN3rgG4DXoJgnclFVw"
+g_key = "<ENTER YOUR KEY HERE>"
