c0fee4186beee8ec26a52bc56dc014749a44a3e9
William F <60886603+wtcf90@users.noreply.github.com>
2020-05-19 16:16:23 -0700

    gitignore addition
    gitignore addition

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "19f617e49bc08fb97137c4f01a36e91d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDJkaNNrL7Cb2jj1a5Qryc2WqNpJk7uVoc"
